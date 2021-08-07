4e1d5edf9faeb12a9e7e5156bad452d1c36902ff
Younesse Boulfaf <yboulfaf@e2r3p1.1337.ma>
2021-08-07 19:15:31 +0100

    added some folders

---
+++ lab/c00/ex01/test01.c
@@ -0,0 +1,7 @@
+#include <unistd.h>
+#include"/Users/yboulfaf/code/c00/ex01/ft_print_alphabet.c"
+int main()
+{
+	ft_print_alphabet();
+	return 0;
+}
---
+++ lab/c00/ex04/test.c
@@ -0,0 +1,7 @@
+#include <unistd.h>
+#include "/Users/yboulfaf/code/c00/ex04/ft_is_negative.c"
+int main()
+{
+	ft_is_negative(4);
+	return (0);
+}
