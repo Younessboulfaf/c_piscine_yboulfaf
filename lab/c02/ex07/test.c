ef7fbde8d313668dfa53027979e6161a79e550cb
Youness Boulfaf <boulfafyouness@gmail.com>
2021-08-15 18:54:05 +0100

    hhhh

---
+++ lab/c02/ex07/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "/Users/yboulfaf/code/c02/ex07/ft_strupcase.c"
+int main ()
+{
+	char  x[]= "aaaa";
+		printf("%s" ,ft_strupcase(x));
+}
---
+++ lab/c02/ex08/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "/Users/yboulfaf/code/c02/ex08/ft_strlowcase.c"
+int main ()
+{
+	char x[]= "AAA";
+	printf("%s" , ft_strlowcase(x));
+}
+
